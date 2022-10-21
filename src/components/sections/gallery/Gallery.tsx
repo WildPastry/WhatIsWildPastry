f58f70c6087ff94e36ebbdb5d040c0bbce0914e3
WildPastry <mykdsn@gmail.com>
2022-10-21 20:39:00 +1300

    Adding styles

--- src/components/sections/gallery/Gallery.tsx
+++ src/components/sections/gallery/Gallery.tsx
@@ -3,13 +3,7 @@ import styles from './Gallery.module.scss';
 const Gallery: React.FC = (): JSX.Element => {
   return (
     <section aria-label='Gallery Section'>
-      <div className={styles.gridContainer}>
-        <div className={styles.item1}><p><strong>Item1</strong>GAL</p></div>
-        <div className={styles.item2}><p><strong>Item2</strong>GAL</p></div>
-        <div className={styles.item3}><p><strong>Item3</strong>GAL</p></div>
-        <div className={styles.item4}><p><strong>Item4</strong>GAL</p></div>
-        <div className={styles.item5}><p><strong>Item5</strong>GAL</p></div>
-      </div>
+      <h1 className='font--thin'>GALLERY</h1>
     </section>
   );
 };
