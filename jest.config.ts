9bb29a4e9d4d34fc40f2038b658257621bb4d78d
WildPastry <mykdsn@gmail.com>
2022-11-08 18:57:20 +1300

    Update jest tests. Add packages.

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,6 @@ import type { JestConfigWithTsJest } from 'ts-jest/dist/types';
 
 const config: JestConfigWithTsJest = {
   preset: 'ts-jest',
-  setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
   testEnvironment: 'jsdom',
   transform: {
     '.+\\.(css|styl|less|sass|scss)$': 'jest-css-modules-transform',
--- src/components/layout/nav/Nav.spec.tsx
+++ src/components/layout/nav/Nav.spec.tsx
@@ -1,8 +1,13 @@
+import { BrowserRouter } from 'react-router-dom';
 import Nav from './Nav';
 import { render } from '@testing-library/react';
 
 describe('<Nav />', () => {
   it('renders the component', () => {
-    render(<Nav />);
+    render(
+      <BrowserRouter>
+        <Nav />
+      </BrowserRouter>
+    );
   });
 });
