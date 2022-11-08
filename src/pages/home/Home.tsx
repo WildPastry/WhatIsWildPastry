f5b645f9cb32f3f6ff5562bbd98184169b95930b
WildPastry <mykdsn@gmail.com>
2022-11-08 19:58:42 +1300

    Add icon styles

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,10 @@
+import { SiAmazons3 } from 'react-icons/si';
+
 const Home: React.FC = (): JSX.Element => {
   return (
     <section aria-label='Home Section' className='container-fluid'>
-      <div className='row'>
-        <div className='col'>
-          <h2 className='font--thin'>GALLERY</h2>
-        </div>
-      </div>
+      <h2 className='font--thin'>GALLERY</h2>
+      <SiAmazons3 className='icon--regular' />
     </section>
   );
 };
